82b56ffa2e73726d3dfa800aaed259ea7936a222
jerryzhoujw <jerryzhoujw@gmail.com>
2019-08-10 21:42:54 +0700

    add Share Extension for device support

---
+++ Share Extension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Share Extension
+//
+//  Created by JerryZhou on 2019/8/10.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
